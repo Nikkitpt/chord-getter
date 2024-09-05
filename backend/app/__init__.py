82dc61df78c3dc780b94910f2a2d15c2664fd331
nikkitpt <ndavis213@gmail.com>
2024-09-05 19:06:04 -0400

    cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,7 +6,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000/"}})
+
 
     # Register your blueprint after CORS setup
     from .routes import main
