ecdb8d1be52112eff3866d495f20b3e74e751bc1
nikkitpt <ndavis213@gmail.com>
2024-09-05 18:29:24 -0400

    updated for tempfile

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,6 +4,7 @@ import librosa
 import numpy as np
 from .utils import extract_chords, allowed_file
 from flask_cors import CORS, cross_origin
+import tempfile
 
 main = Blueprint('main', __name__)
 
@@ -11,23 +12,24 @@ main = Blueprint('main', __name__)
 @main.route('/upload', methods=['POST','OPTIONS'])
 def upload_file():
     print("The Request \n", request.files)
-    # if 'file' not in request.files:
-    #     return jsonify({'error': 'No file part'}), 400
 
-    file = request.files['file']
+    file = request.files.get('file')
 
-    if file.filename == '':
+    if file is None or file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
     if file and allowed_file(file.filename):
-        filename = file.filename
-   
-
-        # file_path = os.path.join(os.getenv('UPLOAD_FOLDER'), filename)
-        file_path = './uploads/audio.mp3'
-        # file.save(file_path)
-
-        chords = extract_chords(file_path)
-        return jsonify({'chords': chords}), 200
+        # Create a temporary file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+            file.save(temp_file.name)
+            temp_file_path = temp_file.name
+        
+        try:
+            # Process the file
+            chords = extract_chords(temp_file_path)
+            return jsonify({'chords': chords}), 200
+        finally:
+            # Clean up the temp file after processing
+            os.remove(temp_file_path)
 
     return jsonify({'error': 'Invalid file type'}), 400
